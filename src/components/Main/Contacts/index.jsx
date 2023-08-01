11cd9631e5b91cefcb77aeb326ba43f398e9ad1f
joab bitencourt <joabbitencourt8@gmail.com>
2023-08-01 10:18:16 -0300

    função copiar email

--- src/components/Main/Contacts/index.jsx
+++ src/components/Main/Contacts/index.jsx
@@ -1,26 +1,42 @@
-import LetsTalkButton from '@/components/LetsTalkButton'
-import SectionTitle from '@/components/SectionTitle'
-import React from 'react'
-import { FaRegPaperPlane } from 'react-icons/fa'
-import { PiCopySimple } from 'react-icons/pi'
-import { AiOutlineArrowUp } from 'react-icons/ai'
+import LetsTalkButton from "@/components/LetsTalkButton";
+import SectionTitle from "@/components/SectionTitle";
+import React from "react";
+import { FaRegPaperPlane } from "react-icons/fa";
+import { PiCopySimple } from "react-icons/pi";
+import { AiOutlineArrowUp } from "react-icons/ai";
 
 export default function Contacts() {
+  function myFunction() {
+    navigator.clipboard.writeText("joabbitencourt8@gmail.com");
+
+    alert("Texto copiado");
+  }
+
   return (
-    <section id="contact" className='contacts flex flex-col justify-center my-20 items-center'>
-      <SectionTitle >📮Contatos</SectionTitle>
-      <h2 className='text-6xl font-bold my-10 text-center'>Vamos Conversar</h2>
+    <section
+      id="contact"
+      className="contacts flex flex-col justify-center my-20 items-center"
+    >
+      <SectionTitle>📮Contatos</SectionTitle>
+      <h2 className="text-6xl font-bold my-10 text-center">Vamos Conversar</h2>
 
       <div className="contacts__contatent flex gap-20 items-center flex-col md:flex-row">
         <LetsTalkButton />
         <div className="contacts__email flex flex-col items-center">
           <FaRegPaperPlane />
           <span>E-mail</span>
-          <span>joabbitencourt8@gmail.com</span>
-          <PiCopySimple />
+          <span id="myInput">joabbitencourt8@gmail.com</span>
+          <button onClick={myFunction}><PiCopySimple /></button>
+          
         </div>
       </div>
-      <a href="#home" className='flex items-center my-20 text-violet hover:neutral-200'> Voltar ao topo <AiOutlineArrowUp /> </a>
+      <a
+        href="#home"
+        className="flex items-center my-20 text-violet hover:neutral-200"
+      >
+        {" "}
+        Voltar ao topo <AiOutlineArrowUp />{" "}
+      </a>
     </section>
-  )
+  );
 }
