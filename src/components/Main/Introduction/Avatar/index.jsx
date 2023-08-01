b5fbbd471da9182ac7dcf126e24e59c6a3378cd8
joab bitencourt <joabbitencourt8@gmail.com>
2023-08-01 09:15:52 -0300

    ajustando avatar

--- src/components/Main/Introduction/Avatar/index.jsx
+++ src/components/Main/Introduction/Avatar/index.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Avatar() {
   return (
     <div className='introduction__avatar flex justify-center'>
-      <div className="introduction__avatar__text flex justify-center">
+      {/* <div className="introduction__avatar__text flex justify-center">
         <span className='introduction__avatar__saudation'>O</span>
         <span className='introduction__avatar__saudation'>l</span>
         <span className='introduction__avatar__saudation'>á</span>
         <span className='introduction__avatar__saudation'>!</span>
-      </div>
+      </div> */}
     </div>
   )
 }
